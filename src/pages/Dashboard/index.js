78be3ddeec4e9b8409105f9e07969ec5826fc4bf
Bexruz00 <barnoyevbexruz140@gmail.com>
2025-03-20 16:03:32 +0500

    Your commit message

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,9 @@
+"use client";
 import Home from "../Dashboard/Home";
 import Group from "./Group/Group";
 import Market from "../Dashboard/Market";
 import Students from "./Students/Students";
-import Teachers from "../Dashboard/teachers";
+import Teachers from "../Dashboard/Teachers";
 import StudentsCrud from "./Students/StudentsCrud";
 import StudentMore from "./Students/StudentMore";
 import TeachersCrud from "./teachersCrud";
